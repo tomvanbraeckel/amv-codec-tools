3e535861b36d8d1dddd72055f836b78046e91efb
voroshil <voroshil@dc3f452a-b039-0410-9ab0-c7f6655b96e0>
2007-10-26 16:00:47 +0000

    Rename functions to comply with implemented codec.

--- G.729/g729.h
+++ G.729/g729.h
@@ -2,11 +2,11 @@
 #define G729A_H
 
 
-int g729_encoder_init();
-int g729_decode_frame(short* ibuf, int ibuflen, short* obuf, int obuflen);
+int g729a_encoder_init();
+int g729a_decode_frame(short* ibuf, int ibuflen, short* obuf, int obuflen);
 
-int g729_decoder_init();
-int g729_encode_frame(short* ibuf, int ibuflen, short* obuf, int obuflen);
+int g729a_decoder_init();
+int g729a_encode_frame(short* ibuf, int ibuflen, short* obuf, int obuflen);
 
 #endif //G729A_H
 
