851ba6787976fb3a3b553bb32c3720dbcad20796
voroshil <voroshil@dc3f452a-b039-0410-9ab0-c7f6655b96e0>
2008-03-01 18:35:30 +0000

    Make code bit-equal to reference

--- G.729/g729a_native.c
+++ G.729/g729a_native.c
@@ -1279,22 +1279,21 @@ static void get_lsp_coefficients(const int16_t* lsp, int* f)
 {
     int i, j;
     int qidx=2;
-    int b;
 
     f[0] = 0x1000000;          // 1.0 in Q24
     f[1] = -lsp[0] << 10;      // *2 and Q15 -> Q24
 
     for(i=2; i<=5; i++)
     {
-        b = -lsp[qidx] << 1;
         f[i] = f[i-2];
 
         for(j=i; j>1; j--)
-            f[j] += mul_24_15(f[j-1], b) + f[j-2];
+            f[j] -= (mul_24_15(f[j-1], lsp[qidx])<<1) - f[j-2];
 
-        f[1] += b << 9;
+        f[1] -= lsp[qidx]  << 10;
         qidx+=2;
     }
+printf("lsf: "); for(i=0; i<6; i++) printf("%d ",f[i]); printf("\n==\n");
 }
 /**
  * \brief LSP to LP conversion (3.2.6)
